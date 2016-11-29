bbcc9e58ef1a26a7838982961128599c58621aac
Peter Iskandar <piskandar@pivotal.io>
2016-11-29 12:50:19 -0500

    initial commit

---
+++ src/main/java/com/test/sleuth/SleuthDemoApplication.java
@@ -0,0 +1,12 @@
+package com.test.sleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthDemoApplication.class, args);
+	}
+}
